f752350f5b1b885de0f5da3073128e3fc2ca06b3
IIIMAXIMIII <145893187+IIIMAXIMIII@users.noreply.github.com>
2025-02-25 18:46:05 +0500

    5 задание

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let field = [
+    [EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY] ];
+
+let current = ZERO;
+
 startGame();
 addResetListener();
 
@@ -27,13 +34,84 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
+    const targetCell = findCell(row, col);
+
+    if (targetCell.textContent !== EMPTY) {
+        return;
+    }
+
+    renderSymbolInCell(current, row, col);
+    field[row][col] = current;
+    current = current === ZERO ? CROSS : ZERO;
     console.log(`Clicked on cell: ${row}, ${col}`);
+
+    let win = haveWin();
+    if (win !== EMPTY) {
+        alert(`Победил ${win}`)
+    }
+
+    if (isEndGame()) {
+        alert("Победила дружба")
+    }
 
 
     /* Пользоваться методом для размещения символа в клетке так:
         renderSymbolInCell(ZERO, row, col);
      */
+}
+
+function getTransponse() {
+    let newField = [
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY] ];
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            newField[i][j] = field[j][i];
+        }
+    }
+
+    return newField;
+}
+
+function haveWin () {
+    for (const row of field) {
+        if (row[0] === row[1] && row[1] === row[2]) {
+            return row[0]
+        }
+    }
+
+    let transponseField = getTransponse();
+
+    for (const row of transponseField) {
+        if (row[0] === row[1] && row[1] === row[2]) {
+            return row[0]
+        }
+    }
+
+    if (field[0][0] === field[1][1] && field[1][1] === field[2][2]) {
+        return field[0][0]
+    }
+
+    if (field[0][2] === field[1][1] && field[1][1] === field[2][0]) {
+        return field[0][2]
+    }
+
+    return EMPTY
+}
+
+function isEndGame () {
+    let count = 0;
+
+    for (const row of field) {
+        for (const cell of row) {
+            if (cell !== EMPTY) {
+                count++;
+            }
+        }
+    }
+
+    return count === 9;
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
